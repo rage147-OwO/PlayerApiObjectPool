4f48830084a62e842e23699782ae63fafce28a50
rage147-OwO <bbbaa1004@gmail.com>
2022-02-06 15:17:22 +0900

    Set

---
+++ rageNetworkSys/NetworkSysMain.cs
@@ -0,0 +1,55 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+[RequireComponent(typeof(VRCObjectPool))]
+
+public class NetworkSysMain : UdonSharpBehaviour
+{
+    [System.NonSerialized] public VRCObjectPool pool;
+    [System.NonSerialized]public byte PlayerObjectNum = 254;
+    private void Start()
+    {
+        pool = ((VRCObjectPool)GetComponent(typeof(VRCObjectPool)));
+    }
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
+        {
+            if (player.isLocal)       //마스터가 처음 월드에 들어왔을때 실행
+            {
+                pool.TryToSpawn();
+                PlayerObjectNum = 0;
+            }
+            else
+            {
+                if (VRCPlayerApi.GetPlayerCount() > pool.Pool.Length)
+                {
+                    Debug.Log("오브젝트풀 에러! 플레이어가 오브젝트풀의 사이즈보다 많음");
+                }
+                else
+                {
+                    Networking.SetOwner(player, pool.TryToSpawn());
+                }
+            }
+        }
+    }            
+
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
+        {
+            for (int i = 0; i < pool.Pool.Length; i++)
+            {
+                if ((PlayerObjectNum != i) && (pool.Pool[i].activeInHierarchy) && (Networking.IsOwner(pool.Pool[i].gameObject)))
+                {
+                    pool.Return(pool.Pool[i].gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ rageNetworkSys/NetworkSysSub.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class NetworkSysSub : UdonSharpBehaviour
+{
+    public NetworkSysMain main;
+    VRCPlayerApi Owner;
+    void OnEnable()
+    {
+        Owner = Networking.GetOwner(this.gameObject);
+        if (Owner.isLocal && !Owner.isMaster)
+        {
+            for (int i = 0; i < main.pool.Pool.Length; i++)
+            {
+                if (this.gameObject == main.pool.Pool[i].gameObject)
+                {
+                    main.PlayerObjectNum = (byte)i;
+                }
+            }
+        }
+    }
+}
